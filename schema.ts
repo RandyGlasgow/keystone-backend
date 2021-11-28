2fd144dabaf6c32fa64dcab7b299ef863b9ad6fc
randyglasgow <glasgowrandy@gmail.com>
2021-11-28 11:51:02 -0500

    removes tasks

--- schema.ts
+++ schema.ts
@@ -12,22 +12,7 @@ import {
 } from '@keystone-next/keystone/fields';
 
 export const lists = {
-  Task: list({
-    fields: {
-      label: text({ isRequired: true }),
-      priority: select({
-        dataType: 'enum',
-        options: [
-          { label: 'Low', value: 'low' },
-          { label: 'Medium', value: 'medium' },
-          { label: 'High', value: 'high' },
-        ],
-      }),
-      isComplete: checkbox(),
-      assignedTo: relationship({ ref: 'Person.tasks', many: false }),
-      finishBy: timestamp(),
-    },
-  }),
+ 
   Person: list({
     fields: {
       name: text({ isRequired: true }),
@@ -39,7 +24,6 @@ export const lists = {
       // we want to ensure that all people have a password set, so we use
       // the isRequired flag.
       password: password({ isRequired: true }),
-      tasks: relationship({ ref: 'Task.assignedTo', many: true }),
     },
   }),
 };
